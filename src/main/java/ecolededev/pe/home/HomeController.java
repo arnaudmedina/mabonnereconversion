6685e498d52b2df05cb371e67af795d474007033
Utilisateur-SJD <Utilisateur-SJD@172.17.4.79>
2017-06-20 16:29:30 +0200

    Ajout de la construction de la liste des métiers avec thymLeaf

--- src/main/java/ecolededev/pe/home/HomeController.java
+++ src/main/java/ecolededev/pe/home/HomeController.java
@@ -1,13 +1,23 @@
 package ecolededev.pe.home;
 
 import java.security.Principal;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import ecolededev.pe.domaine.Metier;
+import ecolededev.pe.services.IMetiersServices;
+
 
 @Controller
 class HomeController {
+
+	@Autowired
+	private IMetiersServices metiersService;
 
 	@ModelAttribute("module")
 	String module() {
@@ -15,7 +25,18 @@ class HomeController {
 	}
 
 	@GetMapping("/")
-	String index(Principal principal) {
-		return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
+	String index(Principal principal, Model model) {
+		if (principal != null) {
+			return "home/homeSignedIn";
+		} else {
+			HomeForm homeForm = (HomeForm) model.asMap().get("homeForm");
+			if (homeForm == null) {
+				homeForm = new HomeForm();
+				model.addAttribute("homeForm", homeForm) ;
+				List<Metier> metiersList = metiersService.listeMetiers();
+				homeForm.setListeMetiers(metiersList);
+			}
+			return "home/homeNotSignedIn";
+		}
 	}
 }
