64202fe6082ae180d880f0b3b6c8a65867a27626
abdel93300 <abdelhafid93300@gmail.com>
2017-08-24 18:55:49 +0200

    Merge Etat Civil (ANE) + Formation (PCO)

--- src/main/java/ecolededev/pe/account/Account.java
+++ src/main/java/ecolededev/pe/account/Account.java
@@ -15,14 +15,8 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-<<<<<<< HEAD
 import ecolededev.pe.domaine.Commune;
 import ecolededev.pe.domaine.DetailFormation;
-=======
-import ecolededev.pe.domaine.AccountFormation;
-import ecolededev.pe.domaine.Commune;
->>>>>>> branch 'master' of https://github.com/arnaudmedina/mabonnereconversion.git
-import ecolededev.pe.domaine.Mobilite;
 import ecolededev.pe.domaine.Situation;
 
 @SuppressWarnings("serial")
@@ -49,13 +43,10 @@ public class Account implements java.io.Serializable {
 	@Column(unique = true)
 	private String email;
 
-<<<<<<< HEAD
 	
 	@Column
 	private String telephone;
 		
-=======
->>>>>>> branch 'master' of https://github.com/arnaudmedina/mabonnereconversion.git
 	@JsonIgnore
 	private String password;
 
@@ -68,10 +59,9 @@ public class Account implements java.io.Serializable {
 	
 	@OneToOne
 	private Situation situation;
-<<<<<<< HEAD
 	
-	@OneToOne
-	private Mobilite mobilite;
+	@Column(name="mobilite")
+	private String mobilite;
 	
 	@OneToMany (mappedBy = "account")
     private List<DetailFormation> detailFormations ;
@@ -84,15 +74,6 @@ public class Account implements java.io.Serializable {
 		this.detailFormations = detailFormations;
 	}
 
-=======
-
-	@OneToOne
-	private Mobilite mobilite;
-	
-	@OneToMany (targetEntity=AccountFormation.class)
-	private List<AccountFormation> accountFormation;
-	
->>>>>>> branch 'master' of https://github.com/arnaudmedina/mabonnereconversion.git
 	public Account() {
 
 	}
@@ -102,24 +83,17 @@ public class Account implements java.io.Serializable {
 		this.password = password;
 		this.role = role;
 		this.created = Instant.now();
-<<<<<<< HEAD
 		
-=======
-
->>>>>>> branch 'master' of https://github.com/arnaudmedina/mabonnereconversion.git
 	}
 
 	public Long getId() {
 		return id;
 	}
 
-<<<<<<< HEAD
     public void setId(Long id) {
 		this.id = id;
 	}
 
-=======
->>>>>>> branch 'master' of https://github.com/arnaudmedina/mabonnereconversion.git
 	public String getEmail() {
 		return email;
 	}
@@ -148,7 +122,6 @@ public class Account implements java.io.Serializable {
 		return created;
 	}
 
-<<<<<<< HEAD
 	public String getNom() {
 		return nom;
 	}
@@ -189,13 +162,11 @@ public class Account implements java.io.Serializable {
 		this.commune = commune;
 	}
 
-	
-
-	public Mobilite getMobilite() {
+	public String getMobilite() {
 		return mobilite;
 	}
 
-	public void setMobilite(Mobilite mobilite) {
+	public void setMobilite(String mobilite) {
 		this.mobilite = mobilite;
 	}
 
@@ -214,80 +185,6 @@ public class Account implements java.io.Serializable {
 	public void setTelephoneMobile(String telephoneMobile) {
 		this.telephoneMobile = telephoneMobile;
 	}
-	
-=======
-	public Commune getCommune() {
-		return commune;
-	}
-
-	public void setCommune(Commune commune) {
-		this.commune = commune;
-	}
-
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-		public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Situation getSituation() {
-		return situation;
-	}
-
-	public void setSituation(Situation situation) {
-		this.situation = situation;
-	}
-
-	public String getTelephoneFixe() {
-		return telephoneFixe;
-	}
-
-	public void setTelephoneFixe(String telephoneFixe) {
-		this.telephoneFixe = telephoneFixe;
-	}
-
-	public String getTelephoneMobile() {
-		return telephoneMobile;
-	}
-
-	public void setTelephoneMobile(String telephoneMobile) {
-		this.telephoneMobile = telephoneMobile;
-	}
-
-	public Mobilite getMobilite() {
-		return mobilite;
-	}
-
-	public void setMobilite(Mobilite mobilite) {
-		this.mobilite = mobilite;
-	}
-
-	public List<AccountFormation> getAccountFormation() {
-		return accountFormation;
-	}
-
-	public void setAccountFormation(List<AccountFormation> accountFormation) {
-		this.accountFormation = accountFormation;
-	}
-
-	public void setCreated(Instant created) {
-		this.created = created;
-	}
->>>>>>> branch 'master' of https://github.com/arnaudmedina/mabonnereconversion.git
 	
 	
 }
