0f38413fe69da4e4a244085ccd6cffa54fb6d886
abdel93300 <abdelhafid93300@gmail.com>
2017-08-24 19:22:57 +0200

    Merge Etat Civil (ANE) + Formation (PCO)

--- src/main/java/ecolededev/pe/home/ProfilForm.java
+++ src/main/java/ecolededev/pe/home/ProfilForm.java
@@ -2,7 +2,6 @@ package ecolededev.pe.home;
 
 import java.util.List;
 
-<<<<<<< HEAD
 import ecolededev.pe.domaine.DetailFormation;
 import ecolededev.pe.domaine.NomFormation;
 import ecolededev.pe.account.Account;
@@ -94,81 +93,6 @@ public class ProfilForm
 		this.codePostal = codePostal;
 	}
 	
-=======
-import ecolededev.pe.account.Account;
-import ecolededev.pe.domaine.Commune;
-import ecolededev.pe.domaine.Mobilite;
-import ecolededev.pe.domaine.Situation;
-
-
-
-public class ProfilForm
-{
-	private Account account;
-	private String situation;
-	private String codeMobilite;
-	private List<Situation> listeSituation;
-	private List<Commune> listeCommune;
-	private List<Mobilite> listeMobilite;
-	private String codePostal;  
-
-	public List<Commune> getListeCommune() {
-		return listeCommune;
-	}
-
-	public String getCodePostal() {
-		return codePostal;
-	}
-
-	public void setCodePostal(String codePostal) {
-		this.codePostal = codePostal;
-	}
-
-	public void setListeCommune(List<Commune> listeCommune) {
-		this.listeCommune = listeCommune;
-	}
-
-	public Account getAccount() {
-		return account;
-	}
-
-	public void setAccount(Account account) {
-		this.account = account;
-	}
-
-	public String getSituation() {
-		return situation;
-	}
-
-	public void setSituation(String situation) {
-		this.situation = situation;
-	}
-
-	public List<Situation> getListeSituation() {
-		return listeSituation;
-	}
-
-	public void setListeSituation(List<Situation> listeSituation) {
-		this.listeSituation = listeSituation;
-	}
-
-	public String getCodeMobilite() {
-		return codeMobilite;
-	}
-
-	public void setCodeMobilite(String codeMobilite) {
-		this.codeMobilite = codeMobilite;
-	}
-
-	public List<Mobilite> getListeMobilite() {
-		return listeMobilite;
-	}
-
-	public void setListeMobilite(List<Mobilite> listeMobilite) {
-		this.listeMobilite = listeMobilite;
-	}
-
-
-
->>>>>>> branch 'master' of https://github.com/arnaudmedina/mabonnereconversion.git
+	
+    	
 }
