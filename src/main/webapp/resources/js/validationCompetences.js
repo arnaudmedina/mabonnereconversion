8e83c5f723e21551698e7e00551a7ab605a57447
arnaudmedina <arnaud.medina@pole-emploi.fr>
2017-10-24 18:03:16 +0200

    Remplacement de l'input expertise par un champ "etoiles"

--- src/main/webapp/resources/js/validationCompetences.js
+++ src/main/webapp/resources/js/validationCompetences.js
@@ -8,8 +8,8 @@ valeursControleCompetence = {
 		annee1: false
 
 },
-//Par fainénantise et pour reduire la taille de mon code,j'ai réuni tous les noms de champs dans ce tableau
-champs = ['level1a5','duree','annee1' ];
+//Par fainénantise et pour reduire la taille de mon code,j'ai réuni tous les noms de champsCompetences dans ce tableau
+champsCompetences = ['level1a5','duree','annee1' ];
 var ladate=new Date();
 //Fonction unique de vérification du formulaire
 function changeListener(event, firstCall) {
@@ -62,13 +62,13 @@ function changeListener(event, firstCall) {
 // Ici j'attends que le document me dise qu'il est complétement chargé
 //if ('complete' === document.readyState) {
 /**
- * Ici je parcours tous mes champs les uns après les autres sans avoir besoin de recopier x fois le même code
+ * Ici je parcours tous mes champsCompetences les uns après les autres sans avoir besoin de recopier x fois le même code
  * Moins de code dupliqué
  *  = moins de code à maintenir
  *  = moins de risque de bug dans le temps
  */
 function pageModaleChargee (){
-for (let champ of champs) {
+for (let champ of champsCompetences) {
 	let inputTmp = document.getElementById(champ);
 
 	inputTmp.addEventListener('change', changeListener);
